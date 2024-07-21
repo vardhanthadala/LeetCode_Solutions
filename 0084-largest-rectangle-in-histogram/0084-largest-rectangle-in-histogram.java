213522312f60d6a8c33dbc73710ed6da680dbda1
vardhanthadala <73521043+vardhanthadala@users.noreply.github.com>
2024-07-21 17:53:17 +0530

    Time: 271 ms (5%), Space: 55.3 MB (99.1%) - LeetHub

---
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -0,0 +1,55 @@
+import java.util.Stack;
+
+class Solution {
+    public static int largestRectangleArea(int[] heights) {
+        Stack<Integer> s = new Stack<>();
+        int maxArea = 0;
+        int nsl[] = new int[heights.length];
+        int nsr[] = new int[heights.length];
+        
+        for (int i = 0; i < heights.length; i++) {
+            while (!s.isEmpty() && heights[s.peek()] >= heights[i]) {
+                s.pop();
+            }
+            
+            if (s.isEmpty()) {
+                nsl[i] = -1;
+            } else {
+                nsl[i] = s.peek();
+            }
+            
+            s.push(i);
+        }
+        
+        s.clear(); // Clear the stack for the next loop
+        
+        for (int i = heights.length - 1; i >= 0; i--) {
+            while (!s.isEmpty() && heights[s.peek()] >= heights[i]) {
+                s.pop();
+            }
+            
+            if (s.isEmpty()) {
+                nsr[i] = heights.length;
+            } else {
+                nsr[i] = s.peek();
+            }
+            
+            s.push(i);
+        }
+        
+        for (int i = 0; i < heights.length; i++) {
+            int height = heights[i];
+            int width = nsr[i] - nsl[i] - 1;
+            int currArea = height * width;
+            maxArea = Math.max(currArea, maxArea);
+        }
+        
+        return maxArea;
+    }
+    
+    public static void main(String args[]) {
+        int heights[] = {2, 1, 5, 6, 2, 3};  // heights in the histogram
+        int maxArea = largestRectangleArea(heights);
+        System.out.println("Maximum area: " + maxArea);
+    }
+}
