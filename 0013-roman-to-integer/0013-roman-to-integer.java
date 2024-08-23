a460b5f04fc643a46cfae53d2f937ddde6cfe39f
vardhanthadala <73521043+vardhanthadala@users.noreply.github.com>
2024-08-23 22:51:27 +0530

    Time: 5 ms (57.76%), Space: 44.7 MB (35.35%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,27 +1,24 @@
 class Solution {
     public int romanToInt(String s) {
-        HashMap<Character, Integer> result = new HashMap<>();
-        result.put('I', 1);
-        result.put('V', 5);
-        result.put('X', 10);
-        result.put('L', 50);
-        result.put('C', 100);
-        result.put('D', 500);
-        result.put('M', 1000);
+        Map<Character, Integer> romanValues = new HashMap<>();
+        romanValues.put('I', 1);
+        romanValues.put('V', 5);
+        romanValues.put('X', 10);
+        romanValues.put('L', 50);
+        romanValues.put('C', 100);
+        romanValues.put('D', 500);
+        romanValues.put('M', 1000);
 
-int val = 0;
- for (int i = 0; i < s.length() - 1; i++) {
-    if (result.get(s.charAt(i)) < result.get(s.charAt(i + 1))) {
-                val -= result.get(s.charAt(i));
-            } else {
-                val += result.get(s.charAt(i));
-            }
+     int result = 0;
+     for(int i=s.length()-1;i>=0;i--){
+        int currVal=romanValues.get(s.charAt(i));
+
+        if(i<s.length()-1 && currVal<romanValues.get(s.charAt(i+1))){
+            result-=currVal;
+        }else{
+            result+=currVal;
         }
-        // Add the value of the last Roman numeral symbol
-        val += result.get(s.charAt(s.length() - 1));
-        
-        return val;
+     }
+     return result;
     }
 }
-
-    
