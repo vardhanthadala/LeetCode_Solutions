1cfcf3acea8f20532816b7165fabef05b63f0cb0
vardhanthadala <73521043+vardhanthadala@users.noreply.github.com>
2024-08-24 01:45:23 +0530

    Time: 10 ms (89.4%), Space: 57.2 MB (69.46%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        if(nums==null||nums.length==0){
+            return false;
+        }
+        HashSet<Integer>set=new HashSet<>();
+        for(int num:nums){
+            if(set.contains(num)){
+                return true;
+            }
+            set.add(num);
+        }
+            return false;
+        
+    }
+}
