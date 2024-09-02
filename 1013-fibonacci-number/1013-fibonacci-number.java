8c0452bb4a89614d841b54998f9e14b1e284115a
vardhanthadala <73521043+vardhanthadala@users.noreply.github.com>
2024-09-02 15:11:54 +0530

    Time: 0 ms (100%), Space: 40 MB (81.78%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -16,15 +16,28 @@ class Solution {
 
         // return dp[n]= fib(n-1) + fib(n-2);
 //-------------------------->Tabulation<--------------------------
-//Tc-O(N)  Sc-O(N)
+    //Tc-O(N)  Sc-O(N)
 
-        if(n<=1) return n;
-            int[]dp=new int[31];
-            dp[0]=1;
-            dp[1]=1;
-            for(int i=2;i<=n;i++) {
-                dp[i]=dp[i-1]+dp[i-2];
-            }
-        return dp[n-1];
+        // if(n<=1) return n;
+        //     int[]dp=new int[31];
+        //     dp[0]=1;
+        //     dp[1]=1;
+        //     for(int i=2;i<=n;i++) {
+        //         dp[i]=dp[i-1]+dp[i-2];
+        //     }
+        // return dp[n-1];
+
+//-------------->space optimization<--------------------------
+//Tc-O(N)  Sc-O(1)
+    if(n<=1) return n;
+    int prev2=0;
+    int prev=1;
+    for(int i=2;i<=n;i++){
+        int curr_i=prev2+prev;
+        prev2=prev;
+        prev=curr_i;
+    }
+
+        return prev;
         }
 }
