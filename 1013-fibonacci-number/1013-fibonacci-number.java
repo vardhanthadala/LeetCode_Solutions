935b42eb6ddd3cf5864a823f562c3d0db58efa42
vardhanthadala <73521043+vardhanthadala@users.noreply.github.com>
2024-09-02 14:47:38 +0530

    Time: 148 ms (9.37%), Space: 44.1 MB (7.16%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,7 +1,20 @@
 class Solution {
     public int fib(int n) { //recursion f(4)= 0 1 1 2 3
-        if(n<=1) return n;
+        // if(n<=1) return n;
 
-        return fib(n-1)+fib(n-2);
-    }
+        // return fib(n-1)+fib(n-2);
+
+               //memoization
+
+        int[]dp=new int[31];
+        Arrays.fill(dp,-1);
+
+        if(n<=1)  return n; //base case
+
+        if(dp[n]!= -1) return dp[n];
+
+        return dp[n]= fib(n-1) + fib(n-2);
+
+
+        }
 }
