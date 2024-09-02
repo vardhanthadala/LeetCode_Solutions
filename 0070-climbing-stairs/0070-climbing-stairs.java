a0727309267f141e08b30f3df4cf38287544522d
vardhanthadala <73521043+vardhanthadala@users.noreply.github.com>
2024-09-02 18:38:07 +0530

    Time: 0 ms (100%), Space: 40.5 MB (27.71%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int climbStairs(int n) {
+    //  ------------->Recursion<-------------Time-O(2^n)
+
+    // if(n==0 ||n==1) return 1;
+
+    // return climbStairs(n-1)+climbStairs(n-2);
+
+//------------------->Tabulation<--------------------------
+
+    if(n==0 || n==1) return 1;
+
+    int[]dp=new int[n+1];
+
+    dp[0]=dp[1]=1;
+
+    for(int i=2;i<=n;i++){
+        dp[i]=dp[i-1]+dp[i-2];
+    }
+    return dp[n];
+    }
+}
