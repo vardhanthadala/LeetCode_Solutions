390096a41dc6499ba115de64b7e214e7f9dfc7fe
vardhanthadala <73521043+vardhanthadala@users.noreply.github.com>
2024-09-02 18:42:03 +0530

    Time: 0 ms (100%), Space: 40 MB (83.6%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -6,17 +6,32 @@ class Solution {
 
     // return climbStairs(n-1)+climbStairs(n-2);
 
-//------------------->Tabulation<--------------------------
+//------------------->Tabulation<--------------------------O(n),O(n)
 
-    if(n==0 || n==1) return 1;
+    // if(n==0 || n==1) return 1;
 
-    int[]dp=new int[n+1];
+    // int[]dp=new int[n+1];
 
-    dp[0]=dp[1]=1;
+    // dp[0]=dp[1]=1;
+
+    // for(int i=2;i<=n;i++){
+    //     dp[i]=dp[i-1]+dp[i-2];
+    // }
+    // return dp[n];
+    //------------------->space optimization<--------------------------
+
+
+    if(n==0||n==1) return 1;
+
+    int prev=1;
+    int curr=1;
 
     for(int i=2;i<=n;i++){
-        dp[i]=dp[i-1]+dp[i-2];
+        int temp=curr;
+        curr=prev+curr;
+        prev=temp;
+
     }
-    return dp[n];
+    return curr;
     }
 }
