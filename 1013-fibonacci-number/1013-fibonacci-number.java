b67dad5ac47c2f1c0a8e58f2cee5df260536faa0
vardhanthadala <73521043+vardhanthadala@users.noreply.github.com>
2024-09-02 15:12:05 +0530

    Time: 0 ms (100%), Space: 40 MB (92.5%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -30,8 +30,10 @@ class Solution {
 //-------------->space optimization<--------------------------
 //Tc-O(N)  Sc-O(1)
     if(n<=1) return n;
+
     int prev2=0;
     int prev=1;
+    
     for(int i=2;i<=n;i++){
         int curr_i=prev2+prev;
         prev2=prev;
