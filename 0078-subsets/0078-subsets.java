aea39e67eed73380aeb44cb6bb69cd4edd302053
vardhanthadala <73521043+vardhanthadala@users.noreply.github.com>
2024-09-11 22:39:37 +0530

    Time: 0 ms (100%), Space: 42.7 MB (33.93%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -2,10 +2,10 @@ class Solution {//2 solns  (1.recursion,2.bit manipulation)
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>>result=new ArrayList<>();
         int n=nums.length;
-        for(int i=0;i<(1<<n);i++){
+        for(int i=0;i<(1<<n);i++){  //1<<n --> number of subsets
             List<Integer>subset=new ArrayList<>();
             for(int j=0;j<n;j++){
-                if((i&(1<<j))>0){
+                if((i&(1<<j))>0){ //set
                     subset.add(nums[j]);
                 }
             }
