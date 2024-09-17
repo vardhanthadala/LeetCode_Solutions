3fc25b1299445351af40fc65327f2596dbce9dbd
vardhanthadala <73521043+vardhanthadala@users.noreply.github.com>
2024-09-17 18:28:32 +0530

    Time: 2 ms (76.51%), Space: 53.3 MB (21.82%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,5 +1,19 @@
 class Solution {
+    //Moore Voting Algorithm
     public int majorityElement(int[] nums) {
-        
+        int majority=0;
+        int count =0;
+
+        for(int num: nums){
+            if(count==0){
+                majority=num;
+            }
+            if(num==majority){
+                count++;
+            }else{
+                count--;
+            }
+        }
+        return majority;
     }
 }
