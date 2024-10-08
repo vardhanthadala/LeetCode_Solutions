87757d08ee3b412a47a3de7705b1cdf5938b8100
vardhanthadala <73521043+vardhanthadala@users.noreply.github.com>
2024-10-08 12:41:04 +0530

    Time: 1 ms (54.73%), Space: 42.7 MB (51.52%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,16 +1,37 @@
-class Solution {//2 solns  (1.recursion,2.bit manipulation)
+class Solution { // 2 solns (1.recursion,2.bit manipulation)
+    List<List<Integer>> ans=new ArrayList<>();
+
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>>result=new ArrayList<>();
-        int n=nums.length;
-        for(int i=0;i<(1<<n);i++){  //1<<n --> number of subsets
-            List<Integer>subset=new ArrayList<>();
-            for(int j=0;j<n;j++){
-                if((i&(1<<j))>0){ //set
-                    subset.add(nums[j]);
-                }
-            }
-            result.add(subset);
+        int n = 0;
+        List<Integer> al = new ArrayList<>(); 
+        allcases(nums, ans, n, al);
+        return ans;
+    }
+
+    public void allcases(int[] nums, List<List<Integer>> ans, int n, List<Integer> al) {
+        if (n == nums.length) {
+            ans.add(new ArrayList<>(al));
+            return;
         }
-        return result;
+        al.add(nums[n]);
+        allcases(nums, ans, n + 1, al);
+        al.remove(al.size() - 1);
+        allcases(nums, ans, n + 1, al);
+        return;
     }
 }
+
+// List<List<Integer>>result=new ArrayList<>();
+// int n=nums.length;
+// for(int i=0;i<(1<<n);i++){ //1<<n --> number of subsets
+// List<Integer>subset=new ArrayList<>();
+// for(int j=0;j<n;j++){
+// if((i&(1<<j))>0){ //set
+// subset.add(nums[j]);
+// }
+// }
+// result.add(subset);
+// }
+// return result;
+// }
+// }
