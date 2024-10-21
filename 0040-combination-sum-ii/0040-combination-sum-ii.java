dc0121c34ba4a2be4924675a8a43492f5b82d0e1
vardhanthadala <73521043+vardhanthadala@users.noreply.github.com>
2024-10-21 23:35:00 +0530

    Time: 2 ms (99.81%), Space: 43.2 MB (57.38%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    private void findCombination(int idx, int[] arr, int target, List<List<Integer>> ans, ArrayList<Integer> ds) {
+        if (target == 0) { // If target is zero, we found a valid combination
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+
+        for (int i = idx; i < arr.length; i++) {
+            if (i > idx && arr[i] == arr[i - 1]) { // Skipping duplicates elements
+                continue;
+            }
+            if (arr[i] > target) { // If current element < target ..we dont need to proceed further..
+                break;
+            }
+
+            ds.add(arr[i]); // Pick the current element
+            findCombination(i + 1, arr, target - arr[i], ans, ds); // Move to the next element
+            ds.remove(ds.size() - 1); // Backtrack to pick another element
+        }
+    }
+
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(candidates); // sorting to handle duplicates
+        findCombination(0, candidates, target, ans, new ArrayList<>());
+        return ans;
+    }
+
+}
