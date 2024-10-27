68bcdc96e232a5ad7c3440c6bfe532fd72b49037
vardhanthadala <73521043+vardhanthadala@users.noreply.github.com>
2024-10-28 00:58:53 +0530

    Time: 11 ms (56.18%), Space: 42.4 MB (75.74%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -11,7 +11,7 @@ class Solution {
             // Checking the character with max number of occurrence
             maxf = Math.max(maxf, arr[s.charAt(r) - 'A']);
 
-            if (r - l + 1 - maxf > k) {
+            if (r - l + 1 - maxf > k) {//r - l + 1 - maxf calculates the number of characters in the current window that are not the most frequent character.//Total characters in the window minus the most frequent character count
                 arr[s.charAt(l) - 'A']--;
                 l++;
             }
