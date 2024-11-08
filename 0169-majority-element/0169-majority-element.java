9b45feed1c8f232fd988b56b9fa02ed8580a9ac1
vardhanthadala <73521043+vardhanthadala@users.noreply.github.com>
2024-11-09 00:12:25 +0530

    Time: 2 ms (79.05%), Space: 53.4 MB (9.35%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,16 +1,36 @@
 class Solution {
-    //Moore Voting Algorithm
-    public int majorityElement(int[] nums) {
-        int majority=0;
-        int count =0;
+    // -----------------1.HashMap O(N) O(N)--------------------
+    // public int majorityElement(int[] nums) {
+    // int n = nums.length;
+    // Map<Integer, Integer> counts = new HashMap<>();
 
-        for(int num: nums){
-            if(count==0){
-                majority=num;
+    // for (int num : nums) {
+    // counts.put(num, counts.getOrDefault(num, 0) + 1);
+    // }
+
+    // for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
+    // if (entry.getValue() > n / 2) {
+    // return entry.getKey();
+    // }
+    // }
+
+    // // Return -1 if no such element is found
+    // return -1;
+    // }
+
+    // }
+    // -------------2.Moore Voting Algorithm O(N) O(1)----------------
+    public int majorityElement(int[] nums) {
+        int majority = 0;
+        int count = 0;
+
+        for (int num : nums) {
+            if (count == 0) {// If count is 0, assign the current element as the new candidate
+                majority = num;
             }
-            if(num==majority){
+            if (num == majority) {// If the current element is the same as the candidate
                 count++;
-            }else{
+            } else {
                 count--;
             }
         }
