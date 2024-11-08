352ff7d805d103f2dd379710142a70bd599778c3
vardhanthadala <73521043+vardhanthadala@users.noreply.github.com>
2024-11-08 23:06:02 +0530

    Time: 4 ms (86.89%), Space: 58.7 MB (16.92%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,13 +1,60 @@
 class Solution {
-    public int findDuplicate(int[] nums) {
-       HashSet<Integer>set=new HashSet<>();
+    // -------------1.HashSet O(n) O(n) --------------------
+    // public int findDuplicate(int[] nums) {
+    // HashSet<Integer> set = new HashSet<>();
 
-       for(int num:nums){
-        if(set.contains(num)){
-            return num;
+    // for (int num : nums) {
+    // if (set.contains(num)) {
+    // return num;
+    // }
+    // set.add(num);
+    // }
+    // return -1;
+    // }
+
+    // }
+
+    // -----------------2.count O(n) O(n)----------------------
+    // public int findDuplicate(int[] nums) {
+    // int len = nums.length;
+    // int count[] = new int[len + 1];
+    // for (int i = 0; i < len; i++) {
+    // count[nums[i]]++;
+    // if (count[nums[i]] > 1) {
+    // return nums[i];
+    // }
+    // }
+    // return len;
+    // }
+    // }
+
+    // --------------3.Sort O(nlogn) O(nlogn--------------------------
+    // public int findDuplicate(int[] nums) {
+    // Arrays.sort(nums);
+    // for (int i = 1; i < nums.length; i++) {
+    // if (nums[i] == nums[i - 1]) {
+    // return nums[i];
+    // }
+    // }
+    // return nums.length;
+    // }
+    // }
+
+    // ----------4.Fast slow pointer O(n) O(1)---------------------
+       public int findDuplicate(int[] nums) {
+        int slow = 0;
+        int fast = 0;
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        slow = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        set.add(num);
-       } 
-       return -1;
+        
+        return slow;
     }
 }
