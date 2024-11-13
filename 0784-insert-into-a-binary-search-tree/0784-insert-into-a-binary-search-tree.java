b8dfab11411d6f420124dab3d0cf2c93ef0003c8
vardhanthadala <73521043+vardhanthadala@users.noreply.github.com>
2024-11-13 23:08:52 +0530

    Time: 0 ms (100%), Space: 45.6 MB (5.25%) - LeetHub

--- 0784-insert-into-a-binary-search-tree/0784-insert-into-a-binary-search-tree.java
+++ 0784-insert-into-a-binary-search-tree/0784-insert-into-a-binary-search-tree.java
@@ -1,29 +1,55 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
 
-    // Recursive O(N)  O(H)
+    // // Recursive O(N) O(H)
+    // public TreeNode insertIntoBST(TreeNode root, int val) {
+    //     if (root == null) {
+    //         return new TreeNode(val);
+    //     }
+    //     if (root.val > val) {
+    //         root.left = insertIntoBST(root.left, val);
+    //     } else {
+    //         root.right = insertIntoBST(root.right, val);
+    //     }
+    //     return root;
+    // }
+
+    // Iterative
     public TreeNode insertIntoBST(TreeNode root, int val) {
-        if(root==null){
-            return new TreeNode(val);
-        }
-        if(root.val>val){
-            root.left=insertIntoBST(root.left,val);
-        }else{
-            root.right=insertIntoBST(root.right,val);
+
+      if(root == null) return new TreeNode(val);
+        
+        TreeNode curr = root;
+        
+        while(true){ // running an infinity loop, look for the place for new node to add
+            if(curr.val < val){
+                if(curr.right != null) curr = curr.right; // update current on right
+                else {
+                    curr.right = new TreeNode(val); // otherwise add current of right to new value TreeNode
+                    break; // breaking this infinity loop
+                }
+            }
+            else{
+                if(curr.left != null) curr = curr.left; // update current on left
+                else{
+                    curr.left = new TreeNode(val); // otherwise add current of left to new value TreeNode
+                    break; // breaking this infinity loop
+                }
+            }
         }
         return root;
     }
