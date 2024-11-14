1a7b0b6b0c28fbde718ed96c8950eec267430ee3
vardhanthadala <73521043+vardhanthadala@users.noreply.github.com>
2024-11-14 19:33:35 +0530

    Time: 117 ms (24.29%), Space: 57.6 MB (16.3%) - LeetHub

---
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/2188-minimized-maximum-of-products-distributed-to-any-store.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minimizedMaximum(int n, int[] quantities) {
+        int low = 1;
+        int high = Arrays.stream(quantities).max().orElse(0);
+        int ans = Integer.MAX_VALUE;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (canAssign(mid, n, quantities)) {
+                ans = Math.min(ans, mid);
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    private boolean canAssign(int mid, int n, int[] quantities) {
+        int count = 0;
+        for (int quantity : quantities) {
+            count += Math.ceil((double) quantity / mid);
+        }
+        return count <= n;
+    }
+}
