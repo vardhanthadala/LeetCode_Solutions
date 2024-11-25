afad42b9335c956d163dae93ec40d5d5ef0b882f
vardhanthadala <73521043+vardhanthadala@users.noreply.github.com>
2024-11-25 15:48:16 +0530

    Time: 1 ms (91.2%), Space: 44.7 MB (97.6%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -0,0 +1,24 @@
+class Solution { 
+//space - visited array+recursion stack space O(N)+O(N)
+// time - O(N)+O(V+2E) 
+    public void dfs(int[][] M, int[] visited, int i) {
+        for (int j = 0; j < M.length; j++) {
+            if (M[i][j] == 1 && visited[j] == 0) {
+                visited[j] = 1;
+                dfs(M, visited, j);
+            }
+        }
+    }
+
+    public int findCircleNum(int[][] isConnected) {
+        int[] visited = new int[isConnected.length];
+        int count = 0;
+        for (int i = 0; i < isConnected.length; i++) {
+            if (visited[i] == 0) {
+                dfs(isConnected, visited, i);
+                count++;
+            }
+        }
+        return count;
+    }
+}
