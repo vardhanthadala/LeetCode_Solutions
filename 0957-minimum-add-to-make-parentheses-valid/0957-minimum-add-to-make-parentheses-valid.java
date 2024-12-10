149c42555cf6a79e0914615c7deb686e34dfc3ae
vardhanthadala <73521043+vardhanthadala@users.noreply.github.com>
2024-12-10 23:29:20 +0530

    Time: 0 ms (100%), Space: 41.7 MB (21.68%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
@@ -1,19 +1,36 @@
 class Solution {
-    public int minAddToMakeValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        int count = 0;
+    // public int minAddToMakeValid(String s) {
+    //     Stack<Character> stack = new Stack<>();
+    //     int count = 0;
 
-        for (char c : s.toCharArray()) {
-            if (c == '(') {
-                stack.push(c);
-            } else if (c == ')' && !stack.isEmpty()) {
-                stack.pop();
-            } else {
-                count++;
+    //     for (char c : s.toCharArray()) {
+    //         if (c == '(') {
+    //             stack.push(c);
+    //         } else if (c == ')' && !stack.isEmpty()) {
+    //             stack.pop();
+    //         } else {
+    //             count++;
+    //         }
+    //     }
+    //         // Count the remaining unmatched opening parentheses
+    //     count += stack.size();
+    //     return count;
+    // }
+    public int minAddToMakeValid(String s){ // using strings T:O(N) S:O(1)
+    int open=0;
+    int close=0;
+
+    for(char c :s.toCharArray()){
+        if(c=='('){
+            open++;
+        }else if(c==')'){
+            if(open>0){
+                open--;
+            }else{
+                close++;
             }
         }
-            // Count the remaining unmatched opening parentheses
-        count += stack.size();
-        return count;
     }
+    return open+close;
+  }
 }
