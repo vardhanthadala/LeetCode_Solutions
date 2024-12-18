00a7b2c91c8ac866eb6ee3dcb375950024e31a8a
Vardhan <73521043+vardhanthadala@users.noreply.github.com>
2024-12-18 10:50:07 +0530

    Time: 3 ms (72.68%), Space: 48.2 MB (38.51%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,86 +1,58 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-//        int candidate1=0;int candidate2=0;
-//        int count1=0;int count2=0;
+       int n=nums.length;
+        int cnt1 = 0, cnt2 = 0; // counts
+        int el1 = Integer.MIN_VALUE; // element 1
+        int el2 = Integer.MIN_VALUE; // element 2
 
-//        for(int num:nums){
-//            if(num==candidate1){
-//                count1++;
-//            }else if(num==candidate2){
-//                count2++;
-//            }else if(count1 ==0){
-//             candidate1=num;
-//              count1=1;
-//            }else if(count2==0){
-//                candidate2=num;
-//                count2=1;
-//            }else{
-//                count1--;
-//                count2--;
-//            }
-//        } 
-//            count1 = 0;
-//     count2 = 0;
-
-//     // Count the occurrences of the candidates
-//     for (int num : nums) {
-//         if (num == candidate1) {
-//             count1++;
-//         } else if (num == candidate2) {
-//             count2++;
-//         }
-//     }
-//     count1 = 0;
-//     count2 = 0;
-
-//     // Count the occurrences of the candidates
-//     for (int num : nums) {
-//         if (num == candidate1) {
-//             count1++;
-//         } else if (num == candidate2) {
-//             count2++;
-//         }
-//     }
-//   // Reset the counters to count the actual occurrences
-//     count1 = 0;
-//     count2 = 0;
-
-//     // Count the occurrences of the candidates
-//     for (int num : nums) {
-//         if (num == candidate1) {
-//             count1++;
-//         } else if (num == candidate2) {
-//             count2++;
-//         }
-//     }
-//        List<Integer>result=new ArrayList<>();
-//        int n=nums.length;
-//        if(count1>n/3){
-//            result.add(candidate1);
-//        }if(count2>n/3){
-//            result.add(candidate2);
-//        }
-//        return result;
-//     }
-
-
-
-        int n = nums.length;
-        List<Integer> result = new ArrayList<>();
-        Map<Integer, Integer> counts = new HashMap<>();
-
-        for (int num : nums) {
-            counts.put(num, counts.getOrDefault(num, 0) + 1);
-        }
-
-        for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
-            if (entry.getValue() > n / 3) {
-                result.add(entry.getKey());
+        // applying the Extended Boyer Moore's Voting Algorithm:
+        for (int i = 0; i < n; i++) {
+            if (cnt1 == 0 && el2 != nums[i]) {
+                cnt1 = 1;
+                el1 = nums[i];
+            } else if (cnt2 == 0 && el1 != nums[i]) {
+                cnt2 = 1;
+                el2 = nums[i];
+            } else if (nums[i] == el1) cnt1++;
+            else if (nums[i] == el2) cnt2++;
+            else {
+                cnt1--; cnt2--;
             }
         }
 
-        return result;
+        List<Integer> ls = new ArrayList<>(); // list of answers
+
+        // el1 and el2 are the majority elements:
+        cnt1 = 0; cnt2 = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == el1) cnt1++;
+            if (nums[i] == el2) cnt2++;
+        }
+
+        int mini = (int)(n / 3) + 1;
+        if (cnt1 >= mini) ls.add(el1);
+        if (cnt2 >= mini) ls.add(el2);
+        
+        return ls;
     }
-
-
 }
+
+// public List<Integer> majorityElement(int[] nums) {// O(N) //O(N)
+// int n = nums.length;
+// List<Integer> result = new ArrayList<>();
+// Map<Integer, Integer> counts = new HashMap<>();
+
+// for (int num : nums) {
+// counts.put(num, counts.getOrDefault(num, 0) + 1);
+// }
+
+// for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
+// if (entry.getValue() > n / 3) {
+// result.add(entry.getKey());
+// }
+// }
+
+// return result;
+// }
+
+// }
