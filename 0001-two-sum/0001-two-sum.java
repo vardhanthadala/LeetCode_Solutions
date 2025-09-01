b891f08a8db8599c4eabd5b3a2b4361456b81824
Vardhan <73521043+vardhanthadala@users.noreply.github.com>
2025-09-01 13:09:49 +0530

    Time: 8 ms (51%), Space: 45 MB (64.58%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -7,31 +7,34 @@ class Solution {
     // for (int i = 0; i < nums.length; i++) {
     // int req = target - nums[i];
     // if (map.containsKey(req)) {
-    // return new int[] { map.get(req), i };
+    // return new int[] { map.get(req), i };// return indices
     // }
     // map.put(nums[i], i);
     // }
 
     // return new int[] { -1, -1 }; // No solution found
     // }
-    // ----------------2.Two pointer O(NlogN) O(1)---------------------
-  
+    // ----------------2.Two pointer  O(N) + O(N*logN)), O(1)---------------------
+
     public int[] twoSum(int[] nums, int target) {
-        int[][] sortedNums = new int[nums.length][2];
 
+        // Storing value + original index
+        int[][] arr = new int[nums.length][2];
         for (int i = 0; i < nums.length; i++) {
-            sortedNums[i] = new int[]{i, nums[i]};
+            arr[i][0] = nums[i]; // Value
+            arr[i][1] = i; // Index
         }
+        //Sort by valueees...
 
-        Arrays.sort(sortedNums, (a, b) -> Integer.compare(a[1], b[1]));
+        Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));//sort  based on the second element ([1]) of each sub-array
 
         int left = 0, right = nums.length - 1;
 
         while (left < right) {
-            int currentSum = sortedNums[left][1] + sortedNums[right][1];
+            int currentSum = arr[left][0] + arr[right][0]; //vals
 
             if (currentSum == target) {
-                return new int[]{sortedNums[left][0], sortedNums[right][0]};
+                return new int[] { arr[left][1], arr[right][1] };//idxs
             } else if (currentSum < target) {
                 left++;
             } else {
@@ -39,6 +42,6 @@ class Solution {
             }
         }
 
-        return new int[]{};
+        return new int[] {};
     }
 }
