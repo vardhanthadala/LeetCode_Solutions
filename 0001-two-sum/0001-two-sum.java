0b79a1c59a25186eb1ae16c4bb040e3bbe15df4e
Vardhan <73521043+vardhanthadala@users.noreply.github.com>
2025-09-01 13:16:43 +0530

    Time: 8 ms (51%), Space: 44.8 MB (89.99%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -15,26 +15,25 @@ class Solution {
     // return new int[] { -1, -1 }; // No solution found
     // }
     // ----------------2.Two pointer  O(N) + O(N*logN)), O(1)---------------------
-
+  
     public int[] twoSum(int[] nums, int target) {
-
         // Storing value + original index
         int[][] arr = new int[nums.length][2];
         for (int i = 0; i < nums.length; i++) {
-            arr[i][0] = nums[i]; // Value
-            arr[i][1] = i; // Index
+            arr[i][0] = i;         // Index
+            arr[i][1] = nums[i];   // Value
         }
-        //Sort by valueees...
 
-        Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));//sort  based on the second element ([1]) of each sub-array
+        //Sort by Valueess...
+        Arrays.sort(arr, (a, b) -> Integer.compare(a[1], b[1]));//sort  based on the second element ([1]) of each sub-array
 
         int left = 0, right = nums.length - 1;
 
         while (left < right) {
-            int currentSum = arr[left][0] + arr[right][0]; //vals
+            int currentSum = arr[left][1] + arr[right][1]; //vals
 
             if (currentSum == target) {
-                return new int[] { arr[left][1], arr[right][1] };//idxs
+                return new int[]{arr[left][0], arr[right][0]};//idxs
             } else if (currentSum < target) {
                 left++;
             } else {
@@ -42,6 +41,6 @@ class Solution {
             }
         }
 
-        return new int[] {};
+        return new int[]{};
     }
 }
