678ce4cf802ebaf9942341e72fc0b43e62ca3e07
Vardhan <73521043+vardhanthadala@users.noreply.github.com>
2025-09-03 18:39:51 +0530

    Time: 15 ms (85.34%), Space: 42.2 MB (78.58%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -4,21 +4,21 @@ class Solution {
             return "";
         }
         
-        int start = 0, end = 0; // Track the start and end indices of the longest palindrome
+        int start = 0, end = 0; 
         
         for (int i = 0; i < s.length(); i++) {
-            // Check for odd-length palindrome centered at i
+            // Check for odd-length palindrome ex: aba
             int len1 = expandAroundCenter(s, i, i);
-            // Check for even-length palindrome centered between i and i + 1
+            // Check for even-length palindrome centered between i and i + 1  ex: abba
             int len2 = expandAroundCenter(s, i, i + 1);
             
-            // Take the longer of the two
+            
             int len = Math.max(len1, len2);
             
-            // Update the start and end indices if the current palindrome is longer
-            if (len > end - start) {
-                start = i - (len - 1) / 2;
-                end = i + len / 2;
+        
+            if (len > end - start) { //end - start is the current longest palindrome length - 1 
+                start = i - (len - 1) / 2; //starting index of palindrome
+                end = i + len / 2;//ending index of palindrome
             }
         }
         
