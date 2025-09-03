20fd41e7d24cd14ee2de5614a4537588187f14a2
Vardhan <73521043+vardhanthadala@users.noreply.github.com>
2025-09-03 18:55:57 +0530

    Time: 6 ms (98.73%), Space: 48.1 MB (43.91%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,18 +1,17 @@
-import java.util.*;
 
 class Solution {
-    public static List<List<String>> groupAnagrams(String[] strs) {
+    public static List<List<String>> groupAnagrams(String[] strs) {//O(N · K log K) O(N.K)
         Map<String, List<String>> anagramGroups = new HashMap<>();
 
         for (String str : strs) {
-            char[] charArray = str.toCharArray();
-            Arrays.sort(charArray);
+            char[] charArray = str.toCharArray();//Step1 : convert str -> char
+            Arrays.sort(charArray);//Step2: Sort the charArray
             String sortedStr = new String(charArray);
 
-            if (!anagramGroups.containsKey(sortedStr)) {
-                anagramGroups.put(sortedStr, new ArrayList<>());
+            if (!anagramGroups.containsKey(sortedStr)) {//check if already present or not
+                anagramGroups.put(sortedStr, new ArrayList<>());//if not create  a new list
             }
-            anagramGroups.get(sortedStr).add(str);
+            anagramGroups.get(sortedStr).add(str);//if already present then add curr str to existed str list
         }
 
         List<List<String>> result = new ArrayList<>();
