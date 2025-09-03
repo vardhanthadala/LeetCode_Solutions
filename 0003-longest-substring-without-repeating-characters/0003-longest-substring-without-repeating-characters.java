6592b5b4a3194822c21fcefef160fe771f92a341
Vardhan <73521043+vardhanthadala@users.noreply.github.com>
2025-09-03 12:11:36 +0530

    Time: 2 ms (98.82%), Space: 43.5 MB (92.8%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,24 +1,45 @@
 class Solution {
-    public int lengthOfLongestSubstring(String s) {
+    //     public int lengthOfLongestSubstring(String s) {//set + sliding window O(N) )(M)
+    //         int n = s.length();
+    //         int maxLen = 0;
 
-        HashMap<Character, Integer> map = new HashMap<>();
+    //         Set<Character> set = new HashSet<>();
+    //         int left = 0;
+    //         for (int right = 0; right < n; right++) {
+    //             if (!set.contains(s.charAt(right))) { //First time character appearance
+    //                 set.add(s.charAt(right));
+    //                 maxLen = Math.max(maxLen, right - left + 1);
+    //             } else { //if its not first time
+    //                 while (set.contains(s.charAt(right))) {
+    //                     set.remove(s.charAt(left));//remove left character from set
+    //                     left++;
+    //                 }
+    //                 set.add(s.charAt(right)); // add right character in set
 
-        int maxLength = 0;
+    //             }
+    //         }
+    //         return maxLen;
+    //     }
+    // }
+    public int lengthOfLongestSubstring(String s) { //Array + sliding window O(N) O(1)
+        int n = s.length();
+        int maxLen = 0;
+
+        int[] charIdx = new int[128];
+        Arrays.fill(charIdx, -1);
+        //left -> marks start of curr window
+        //right ->marks end of curr window
         int left = 0;
 
-        for (int right = 0; right < s.length(); right++) {
-            char currentChar = s.charAt(right);
-
-            if (map.containsKey(currentChar) && map.get(currentChar) >= left) {// (map.get(currentChar) >= left) ensure that left only moves forward when encountering a character that’s already part of the current substring (between left and right
-
-                left = map.get(currentChar) + 1;
+        for (int right = 0; right < n; right++) {
+            if (charIdx[s.charAt(right)] >= left) { // If character at right was seen inside current window, move left pointer
+                left = charIdx[s.charAt(right)] + 1;
             }
-
-            map.put(currentChar, right);
-
-            maxLength = Math.max(maxLength, right - left + 1);
+            charIdx[s.charAt(right)] = right;//This line updates the array to say: "The character s.charAt(right) was last seen at index right."
+            maxLen = Math.max(maxLen, right - left + 1);
         }
+        return maxLen;
 
-        return maxLength;
     }
+
 }
