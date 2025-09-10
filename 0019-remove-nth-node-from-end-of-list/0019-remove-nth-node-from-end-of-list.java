4a152d6248552fd26b0d992a8e135b253b78b5b4
Vardhan <73521043+vardhanthadala@users.noreply.github.com>
2025-09-10 17:09:42 +0530

    Time: 0 ms (100%), Space: 41.8 MB (45.45%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -13,18 +13,17 @@ class Solution {
         ListNode slow = head;
         ListNode fast = head;
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) { //Fast moves till n
             fast = fast.next;
         }
-        if (fast == null) {
+        if (fast == null) {//if given list size and n is same then fast will be at null...so remove head and return head.next
             return head.next;
         }
-        while (fast.next != null) {
+        while (fast.next != null) { //move slow and fast pointer by one - one step
             fast = fast.next;
             slow = slow.next;
         }
-        slow.next = slow.next.next;// the node n is at after slow pointer thats why removing slow.next and then
-                                   // linking it with next node after n
+        slow.next = slow.next.next;// skipping the n value(deleting the n)
 
         return head;
 
