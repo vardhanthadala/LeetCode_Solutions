966ea82a3e1053e9f8b1bba5399452045f42928b
Vardhan <73521043+vardhanthadala@users.noreply.github.com>
2025-09-24 14:12:33 +0530

    Time: 1 ms (86.97%), Space: 48.1 MB (29%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,24 +1,29 @@
-class Solution { 
-//space - visited array+recursion stack space O(N)+O(N)
-// time - O(N)+O(V+2E) 
-    public void dfs(int[][] M, int[] visited, int i) {
-        for (int j = 0; j < M.length; j++) {
-            if (M[i][j] == 1 && visited[j] == 0) {
-                visited[j] = 1;
-                dfs(M, visited, j);
+class Solution {
+    private static void dfs(int[][] graph, int curr, boolean[] visited) {
+        visited[curr] = true;
+
+        for (int i = 0; i < graph.length; i++) {
+            // If there is a direct connection and the node has not been visited
+            if (graph[curr][i] == 1 && !visited[i]) {
+                //then visit it by recursively
+                dfs(graph, i, visited);
             }
         }
     }
 
     public int findCircleNum(int[][] isConnected) {
-        int[] visited = new int[isConnected.length];
-        int count = 0;
-        for (int i = 0; i < isConnected.length; i++) {
-            if (visited[i] == 0) {
-                dfs(isConnected, visited, i);
-                count++;
+        int n = isConnected.length;
+        boolean[] visited = new boolean[n];
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            //If not been visited, perform DFS from it
+            if (!visited[i]) {
+                dfs(isConnected, i, visited);
+                cnt++;
             }
         }
-        return count;
+        return cnt;
     }
+
 }
