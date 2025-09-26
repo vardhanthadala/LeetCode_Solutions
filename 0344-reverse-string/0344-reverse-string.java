c8e990ac991c3d3ff0d1d60d338ce14aa1b46ebc
Vardhan <73521043+vardhanthadala@users.noreply.github.com>
2025-09-26 17:36:29 +0530

    Time: 0 ms (100%), Space: 49.2 MB (53.61%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,29 +1,29 @@
-// class Solution {
-//     public void reverseString(char[] s) {
-//         int left = 0;
-//         int right = s.length - 1;
-//         while (left <= right) {
-//             char temp = s[left];
-//             s[left] = s[right];
-//             s[right] = temp;
-//             left++;
-//             right--;
-//         }
-//     }
-// }
-
 class Solution {
     public void reverseString(char[] s) {
-        reverse(s, 0, s.length - 1);
-    }
-
-    private void reverse(char[] s, int left, int right) {
-        if (left >= right) {
-            return;
+        int left = 0;
+        int right = s.length - 1;
+        while (left <= right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
         }
-        char temp = s[left];
-        s[left] = s[right];
-        s[right] = temp;
-        reverse(s, left + 1, right - 1);//This line is the heart of recursion — after swapping the first and last characters, we need to move inward and repeat the process
     }
 }
+
+// class Solution {
+//     public void reverseString(char[] s) {
+//         reverse(s, 0, s.length - 1);
+//     }
+
+//     private void reverse(char[] s, int left, int right) {
+//         if (left >= right) {
+//             return;
+//         }
+//         char temp = s[left];
+//         s[left] = s[right];
+//         s[right] = temp;
+//         reverse(s, left + 1, right - 1);//This line is the heart of recursion — after swapping the first and last characters, we need to move inward and repeat the process
+//     }
+// }
