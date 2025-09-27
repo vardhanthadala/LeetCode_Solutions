592ffa7560ba223468a423a76c399c8de4da6e62
Vardhan <73521043+vardhanthadala@users.noreply.github.com>
2025-09-27 22:52:33 +0530

    Time: 6 ms (41.42%), Space: 43.1 MB (82.91%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,17 +1,32 @@
 class Solution {
-    public boolean isAnagram(String s, String t) {
+
+    // public boolean isAnagram(String s, String t) {//HashMap O(N) O(N)
+    //     if (s.length() != t.length()) {
+    //         return false;
+    //     }
+    //     HashMap<Character, Integer> sCount = new HashMap<>();
+    //     HashMap<Character, Integer> tCount = new HashMap<>();
+
+    //     for(int i=0;i<s.length();i++){
+    //         sCount.put(s.charAt(i),sCount.getOrDefault(s.charAt(i),0)+1);
+    //         tCount.put(t.charAt(i),tCount.getOrDefault(t.charAt(i),0)+1);
+    //     }
+    //     return sCount.equals(tCount);
+    // }
+
+    public boolean isAnagram(String s, String t) { //O(N) O(1)
+
+        if (s.length() != t.length())
+            return false;
         int count[] = new int[26];
 
-        for (char x : s.toCharArray()) {
-            count[x - 'a']++;
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++; // Increment for s
+            count[t.charAt(i) - 'a']--; // Decrement for t
         }
-        for (char x : t.toCharArray()) {
-            count[x - 'a']--;
-        }
-        for (int val : count) {
-            if (val != 0) {
-                return false;
-            }
+        for (int c : count) {
+            if (c != 0)
+                return false; // If any count is non-zero, not an anagram
         }
         return true;
     }
