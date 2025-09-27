c10b269cd7cd717fef56bcadd42f53854ed456cb
Vardhan <73521043+vardhanthadala@users.noreply.github.com>
2025-09-27 22:48:12 +0530

    Time: 6 ms (85.38%), Space: 42.9 MB (86.6%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,49 +1,49 @@
-class Solution {//  O(n) , O(n) ->bcuz of used stringbuilder
+class Solution {//  O(n) , O(n) ->bcuz of used stringbuilder  Optimal
+    public String reverseWords(String s) {
+        String[] words = s.trim().split("\\s+");
+        StringBuilder reversed = new StringBuilder();
+        for (int i = words.length - 1; i >= 0; i--) {
+            reversed.append(words[i]);
+            if (i > 0) { //if the current word is not the last word (i > 0), it also appends a space character to separate the words
+                reversed.append(" ");
+            }
+        }
+        return reversed.toString();
+    }
     // public String reverseWords(String s) {
-    //     String[] words = s.trim().split("\\s+");
-    //     StringBuilder reversed = new StringBuilder();
-    //     for (int i = words.length - 1; i >= 0; i--) {
-    //         reversed.append(words[i]);
-    //         if (i > 0) { //if the current word is not the last word (i > 0), it also appends a space character to separate the words
-    //             reversed.append(" ");
+    //     int left = 0;
+    //     int right = s.length() - 1;
+
+    //     String temp = "";
+    //     String ans = "";
+
+    //     while (left <= right) {
+    //         char ch = s.charAt(left);
+
+    //         if (ch != ' ') { //if its a character
+    //             temp += ch; // build the word
+    //         } else if (ch == ' ') {//if its a space
+    //             if (!temp.equals("")) { // only add if word exists
+    //                 if (!ans.equals("")) { //if a word is already present then add curr word before it(Reverse order)
+    //                     ans = temp + " " + ans;
+    //                 } else {
+    //                     ans = temp;
+    //                 }
+    //                 temp = ""; // reset for next word
+    //             }
+    //         }
+    //         left++;
+    //     }
+
+    //     // Add the last word 
+    //     if (!temp.equals("")) {//if last word is not empty
+    //         if (!ans.equals("")) {// if ans already has some words
+    //             ans = temp + " " + ans;
+    //         } else {
+    //             ans = temp;
     //         }
     //     }
-    //     return reversed.toString();
+
+    //     return ans;
     // }
-    public String reverseWords(String s) {
-        int left = 0;
-        int right = s.length() - 1;
-
-        String temp = "";
-        String ans = "";
-
-        while (left <= right) {
-            char ch = s.charAt(left);
-
-            if (ch != ' ') { //if its a character
-                temp += ch; // build the word
-            } else if (ch == ' ') {//if its a space
-                if (!temp.equals("")) { // only add if word exists
-                    if (!ans.equals("")) { //if a word is already present then add curr word before it(Reverse order)
-                        ans = temp + " " + ans;
-                    } else {
-                        ans = temp;
-                    }
-                    temp = ""; // reset for next word
-                }
-            }
-            left++;
-        }
-
-        // Add the last word 
-        if (!temp.equals("")) {//if last word is not empty
-            if (!ans.equals("")) {// if ans already has some words
-                ans = temp + " " + ans;
-            } else {
-                ans = temp;
-            }
-        }
-
-        return ans;
-    }
 }
