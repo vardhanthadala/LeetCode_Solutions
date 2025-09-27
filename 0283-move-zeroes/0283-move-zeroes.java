d3a18132ae7b457e0718e7caf82265e7960fe23c
Vardhan <73521043+vardhanthadala@users.noreply.github.com>
2025-09-27 09:50:52 +0530

    Time: 2 ms (78.88%), Space: 46.3 MB (39.45%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,14 +1,31 @@
 class Solution {
-    public void moveZeroes(int[] nums) {
-        int left=0;
-        for(int right=0;right<nums.length;right++){
-            if(nums[right]!=0){
-                int temp=nums[right];
-                nums[right]=nums[left];
-                nums[left]=temp;
+    // public void moveZeroes(int[] nums) {//O(n) O(n)
+    //     int left=0;
+    //     for(int right=0;right<nums.length;right++){
+    //         if(nums[right] != 0){// Non zero element
+    //             int temp= nums[left];
+    //             nums[left]=nums[right];
+    //             nums[right]=temp;
 
-                left++;
+    //             left++;
+    //         }
+
+    //     }
+    // }
+    public void moveZeroes(int[] nums) {
+        int lastNonZero = 0;
+
+        //Step 1 : Move non zero elements forward
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[lastNonZero] = nums[i];
+                lastNonZero++;
             }
         }
+        // Step 2 : Fill remaining positions with 0
+        for (int i = lastNonZero; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+
     }
 }
