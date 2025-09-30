01782563286faca2b2a1b2e8b3f8ba0ce51b9912
Vardhan <73521043+vardhanthadala@users.noreply.github.com>
2025-09-30 23:16:29 +0530

    Time: 2 ms (66.69%), Space: 41.5 MB (56.03%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,15 +1,16 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        int i=0;
-        int j=0;
-        while(i<s.length()&&j<t.length()){
-            if(s.charAt(i)==t.charAt(j++)){
+        int i = 0;
+        int j = 0;
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
                 i++;
-                
+
             }
-            
+            j++;
+
         }
-        return i==s.length();
+        return i == s.length();
         //If i == s.length(), then all the characters is s were matched to some character in t. Hence s is a subsequence of t.
     }
 }
