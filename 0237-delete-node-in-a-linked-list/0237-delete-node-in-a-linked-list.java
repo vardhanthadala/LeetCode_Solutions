00e77bf533f0c0a5754425cda00a0a7a7158a1ce
Vardhan <73521043+vardhanthadala@users.noreply.github.com>
2025-10-04 15:24:19 +0530

    Time: 0 ms (100%), Space: 44.3 MB (28.62%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.java
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.java
@@ -8,7 +8,14 @@
  */
 class Solution {
     public void deleteNode(ListNode node) {
-    node.val=node.next.val;
+    node.val=node.next.val;//Copy next node’s value
     node.next=node.next.next;  
     }
 }
+
+ /** [a,b,c,d] thn node = b
+  To remove b we should store c in b --> [ a,c,c,d] --> node.val = node.next.val
+  then remove the first c --> [a,c,b]-->node.next=node.next.next
+  
+
+  */
