231ffe5bc1a6f34823fb994c716ceb370ce76402
Vardhan <73521043+vardhanthadala@users.noreply.github.com>
2025-10-06 20:19:11 +0530

    Time: 0 ms (100%), Space: 42.8 MB (66.89%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,38 +1,40 @@
 class Solution { // 2 solns (1.recursion,2.bit manipulation)
-   
-    List<List<Integer>> ans=new ArrayList<>();
+    //     List<List<Integer>> ans=new ArrayList<>();
 
-    public List<List<Integer>> subsets(int[] nums) {
-        int idx = 0;
-        List<Integer> al = new ArrayList<>(); 
-        allcases(nums, ans, idx, al);
-        return ans;
-    }
+    //     public List<List<Integer>> subsets(int[] nums) {
+    //         int n = 0;
+    //         List<Integer> al = new ArrayList<>(); 
+    //         allcases(nums, ans, n, al);
+    //         return ans;
+    //     }
 
-    public void allcases(int[] nums, List<List<Integer>> ans, int idx, List<Integer> al) {
-        if (idx == nums.length) {
-            ans.add(new ArrayList<>(al));
-            return;
+    //     public void allcases(int[] nums, List<List<Integer>> ans, int n, List<Integer> al) { 
+    //         if (n == nums.length) {
+    //             ans.add(new ArrayList<>(al));
+    //             return;
+    //         }
+    //         al.add(nums[n]);                     // include current element
+    //         allcases(nums, ans, n + 1, al);     // explore with this element included
+    //         al.remove(al.size() - 1);          // backtrack (remove last added element)
+    //         allcases(nums, ans, n + 1, al);   // explore without this element
+    //         return;
+    //     }
+    // }
+
+    List<List<Integer>> result = new ArrayList<>();
+
+    public List<List<Integer>> subsets(int[] nums) { //O(N * 2^N)  O(2^n * N)
+        int n = nums.length;
+        int totalSubsets = 1 << n;//1<<n --> number of subsets i.e .., 2^n subsets
+        for (int i = 0; i < totalSubsets; i++) {
+            List<Integer> subset = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                if ((i & (1 << j)) > 0) { //This checks if the j-th bit in the number i is set to 1
+                    subset.add(nums[j]);
+                }
+            }
+            result.add(subset);
         }
-        al.add(nums[idx]);
-        allcases(nums, ans, idx + 1, al);
-        al.remove(al.size() - 1);
-        allcases(nums, ans, idx + 1, al);
-        return;
+        return result;
     }
 }
-
-// List<List<Integer>>result=new ArrayList<>();
-// int n=nums.length;
-// for(int i=0;i<(1<<n);i++){ //1<<n --> number of subsets
-// List<Integer>subset=new ArrayList<>();
-// for(int j=0;j<n;j++){
-// if((i&(1<<j))>0){ //set
-// subset.add(nums[j]);
-// }
-// }
-// result.add(subset);
-// }
-// return result;
-// }
-// }
