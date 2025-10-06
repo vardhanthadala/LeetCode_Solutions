3cab84e9aae9a8f46dfb0af0d06e5fc4ee0864d3
Vardhan <73521043+vardhanthadala@users.noreply.github.com>
2025-10-06 22:52:58 +0530

    Time: 0 ms (100%), Space: 42.3 MB (33.15%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -1,32 +1,49 @@
 class Solution {
-    public int findMin(int[] nums) {
-        int left = 0;
-        int right = nums.length - 1;
+    // public int findMin(int[] nums) { //Binary search
+    //     int left = 0;
+    //     int right = nums.length - 1;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            // Case 1: Array is already sorted (no rotation in this part)
-            if (nums[left] <= nums[right]) {
-                return nums[left];
-            }
-            // Case 2: mid > mid+1 → rotation point is right after mid
-            if (nums[mid] > nums[mid + 1]) {
-                return nums[mid + 1];
-            }
-            // Case 3: mid-1 > mid → rotation point is at mid
+    //     while (left <= right) {
+    //         int mid = left + (right - left) / 2;
+    //         // Case 1: Array is already sorted (no rotation in this part)
+    //         if (nums[left] <= nums[right]) {
+    //             return nums[left];
+    //         }
+    //         // Case 2: mid > mid+1 → rotation point is right after mid
+    //         if (nums[mid] > nums[mid + 1]) {
+    //             return nums[mid + 1];
+    //         }
+    //         // Case 3: mid-1 > mid → rotation point is at mid
 
-            if (nums[mid - 1] > nums[mid]) {
-                return nums[mid];
-            }
-            // Case 4: Decide which half to search
+    //         if (nums[mid - 1] > nums[mid]) {
+    //             return nums[mid];
+    //         }
+    //         // Case 4: Decide which half to search
 
-            if (nums[left] < nums[mid]) {// left half is sorted → min must be in right half
-                left = mid + 1;
-            } else { // right half is sorted → min must be in left half
-                right = mid - 1;
+    //         if (nums[left] < nums[mid]) {// left half is sorted → min must be in right half
+    //             left = mid + 1;
+    //         } else { // right half is sorted → min must be in left half
+    //             right = mid - 1;
+    //         }
+    //     }
+
+    //     return -1;
+    // }
+    public int findMin(int[] nums) { // simple Binary search
+        int n = nums.length;
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] > nums[high]) {
+                low = mid + 1;
+            } else {
+                high = mid;// Minimum is in left half including mid
             }
         }
+        return nums[low]; //finally loww == high
 
-        return -1;
     }
 }
