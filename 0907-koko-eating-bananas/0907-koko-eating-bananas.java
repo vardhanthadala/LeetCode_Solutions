81d1daba276aae6624ea9f78afb05fe469e919f5
Vardhan <73521043+vardhanthadala@users.noreply.github.com>
2025-10-07 13:40:18 +0530

    Time: 6 ms (99.36%), Space: 44.9 MB (96.53%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -9,13 +9,14 @@ class Solution {
         return max;
     }
 
-    private boolean canEatWithinTime(int[] piles, int h, int k) {
+    private boolean canEatWithinTime(int[] piles, int h, int k) {// k is minimum eating speed 
         int totalHours = 0;
         int n = piles.length;
         for (int i = 0; i < n; i++) {
-            totalHours += (piles[i] + k - 1) / k;// k is eating speed //(piles[i] + k - 1) / k used to get ceil number
+            totalHours += (piles[i] + k - 1) / k; //it calculating how many hours Koko needs to eat the i-th pile at speed k, and then adding it to the total hours. ⌈piles[i]/k​⌉ = piles[i]+k−1 / k ​(integer division)
         }
-
+        //totalHours = total time Koko takes to eat all piles at speed k.
+        //h = the total hours Koko has before guards come.
         return totalHours <= h;
     }
 
