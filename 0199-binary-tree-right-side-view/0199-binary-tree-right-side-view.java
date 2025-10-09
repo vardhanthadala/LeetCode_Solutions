045be10a08fac13738b8d70a422f52a20a46bc62
Vardhan <73521043+vardhanthadala@users.noreply.github.com>
2025-10-09 14:22:35 +0530

    Time: 0 ms (100%), Space: 42.3 MB (44.9%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -14,7 +14,34 @@
  * }
  */
 class Solution {
-    public List<Integer> rightSideView(TreeNode root) {
+    /*
+     public List<Integer> rightSideView(TreeNode root) { //bfs
+        List<Integer> result = new ArrayList<>();
+        if (root == null) return result;
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                
+                // if it's the last node in this level, add to result
+                if (i == size - 1) {
+                    result.add(node.val);
+                }
+                
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+            }
+        }
+        
+        return result;
+    }
+
+    */
+    public List<Integer> rightSideView(TreeNode root) { //dfs
         List<Integer> res = new ArrayList<>();
         recursionRight(root, 0, res);
         return res;
@@ -27,7 +54,7 @@ class Solution {
         if (res.size() == level) { // first time visited a level ,so add it to result
             res.add(root.val);
         }
-        recursionRight(root.right, level + 1, res);
+        recursionRight(root.right, level + 1, res); 
         recursionRight(root.left, level + 1, res);
 
     }
