74926038c435d8e4ecbfe2b5cd32246f123cbb6f
Vardhan <73521043+vardhanthadala@users.noreply.github.com>
2025-10-25 11:09:32 +0530

    Time: 1 ms (18.52%), Space: 41.7 MB (36.17%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -3,18 +3,28 @@ class Solution {
     //      return haystack.indexOf(needle);
 
     // }
-    public int strStr(String haystack, String needle) {
-        int haylength = haystack.length();
-        int needlelength = needle.length();
-        if (haylength < needlelength) {
-            return -1;
-        }
-        for (int i = 0; i <= haylength - needlelength; i++) {
-            int j = 0;
-            while (j < needle.length() && haystack.charAt(i + j) == needle.charAt(j))
-                j++;
-            if (j == needle.length()) {
-                return i;
+    // public int strStr(String haystack, String needle) {
+    //     int haylength = haystack.length();
+    //     int needlelength = needle.length();
+    //     if (haylength < needlelength) {
+    //         return -1;
+    //     }
+    //     for (int i = 0; i <= haylength - needlelength; i++) {//haylength - needlelength --> We’re checking every possible starting index i in haystack where needle could fit completely.
+    //         int j = 0;
+    //         while (j < needle.length() && haystack.charAt(i + j) == needle.charAt(j))//if we use :- haystack.charAt(i) thn it does not move with j
+    //             j++;
+    //         if (j == needle.length()) {
+    //             return i;
+    //         }
+    //     }
+    //     return -1;
+    // }
+    public int strStr(String haystack, String needle){
+        for(int i=0;i<haystack.length()-needle.length()+1;i++){
+            if(haystack.charAt(i)==needle.charAt(0)){
+                if(haystack.substring(i,needle.length()+i).equals(needle)){
+                    return i;
+                }
             }
         }
         return -1;
