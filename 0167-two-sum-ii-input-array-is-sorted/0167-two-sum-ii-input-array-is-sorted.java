dcc43a88bbe4406b8dad54b7295ab7efa421d98e
Vardhan <73521043+vardhanthadala@users.noreply.github.com>
2025-10-25 22:59:04 +0530

    Time: 6 ms (15.52%), Space: 47 MB (88.69%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,21 +1,35 @@
 class Solution {
-    public int[] twoSum(int[] numbers, int target) {
-    
-        int left = 0, right = numbers.length - 1;
-        
-        while (left < right) {
-            int sum = numbers[left] + numbers[right];
-            
-            if (sum == target) {
-                return new int[]{left +1, right+1 }; 
-            } else if (sum < target) {
-                left++;
-            } else {
-                right--;
+    // public int[] twoSum(int[] numbers, int target) { // O(n) O(1)
+
+    //     int left = 0, right = numbers.length - 1;
+
+    //     while (left < right) {
+    //         int sum = numbers[left] + numbers[right];
+
+    //         if (sum == target) {
+    //             return new int[]{left +1, right+1 }; 
+    //         } else if (sum < target) {
+    //             left++;
+    //         } else {
+    //             right--;
+    //         }
+    //     }
+
+    //     return new int[]{-1, -1}; 
+
+    // }
+    public int[] twoSum(int[] numbers, int target) { //O(n) O(n)
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < numbers.length; i++) {
+            int req = target - numbers[i];
+
+            if (map.containsKey(req)) {
+                return new int[] { map.get(req), i + 1 };//i+1 for 1 based index
             }
+            map.put(numbers[i], i + 1);//i+1 for 1 based index
         }
-        
-        return new int[]{-1, -1}; 
-    
+
+        return new int[] {};
     }
 }
