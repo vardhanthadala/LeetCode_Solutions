36e9fdd293302b15175cc469a82768d41d7de44e
Vardhan <73521043+vardhanthadala@users.noreply.github.com>
2025-10-27 13:44:12 +0530

    Time: 0 ms (100%), Space: 43.1 MB (7.3%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -7,13 +7,12 @@ class Solution {
         while (low < high) {
             int mid = low + (high - low) / 2;
 
-            if (nums[mid] < nums[mid + 1]) {
-                low = mid + 1;
+            if (nums[mid] > nums[mid + 1]) {
+                high = mid;// Peak lies in left half (including mid)
             } else {
-                high = mid;
+                low = mid + 1;
             }
         }
-
         return low;
     }
 }
